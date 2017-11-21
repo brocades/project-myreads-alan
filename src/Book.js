f168017f1c4d51cd6166cf3e7bdb9808ef309047
Alan Brochier <alanbrc@gmail.com>
2017-11-21 15:03:14 -0200

    type: create, modify and add body: created search page, added route package and modified search input in order to search for results. footer:

--- src/Book.js
+++ src/Book.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import * as BooksAPI from './BooksAPI'
 
 class Book extends Component {
 
@@ -10,34 +9,46 @@ class Book extends Component {
               {value: "none", text:"None"}]
   }
 
-  updateShelf(elementId) {
-    let selectBox = document.getElementById(elementId);
-    let selectedValue = selectBox.options[selectBox.selectedIndex].value;
-    BooksAPI.update(this.props.book, selectedValue).then((msg) => console.log(msg))
+  changeShelf = (elementId) => {
+    let selectBox = document.getElementById(elementId)
+    let selectedValue = selectBox[selectBox.selectedIndex].value
+    this.props.onChangeShelf(this.props.book, selectedValue)
   }
-
 	render() {
 		const { id, title, authors, imageLinks, shelf } = this.props.book
+    //if (this.props.onChangeShelf) console.log("the function exists")
 		return (
 			<div className="book">
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${imageLinks.smallThumbnail}")` }}></div>
           <div className="book-shelf-changer">
-            <select id={id} onchange={this.updateShelf(id)}>
+            <select 
+              id={id} 
+              defaultValue={shelf}
+              onChange={() => this.changeShelf(id)}>
               <option value="none" disabled>Move to...</option>
-              {this.state.options.map((option) => (
+              {this.state.options.map((option, index) => (
                 option.value !== shelf ? 
-                (<option value={option.value}>{option.text}</option>) 
-                : (<option value={option.value}>✓ {option.text}</option>)
+                (<option 
+                  key={index} 
+                  value={option.value}
+                  >{option.text}</option>) 
+                : 
+                (<option
+                  key={index} 
+                  value={option.value}
+                  >✓ {option.text}</option>)
               ))}
               
             </select>
           </div>
         </div>
         <div className="book-title">{title}</div>
-        {authors.map((author) => (
-          <div className="book-authors">{author}</div>
-          ))}
+        { authors instanceof Array ? 
+            authors.map((author, index) => (<div key={index} className="book-authors">{author}</div>))
+            :
+            <div key={authors} className="book-authors">{authors}</div>
+        }             
       </div>
 		)
 	}
